7534309f3e4844e483dc61f232f4d3277eec8863
AndreaCarolinaBaronCaceres <acbaca6789@gmail.com>
2021-07-18 12:25:27 -0500

    Operadores_aritmeticos_14_07_2021

---
+++ main.js
@@ -0,0 +1,81 @@
+
+
+let Nombre =prompt(`Ingrese su nombre`,`L`)
+
+// Resto
+let v = 12
+let z = 6
+console.log ("resto")
+console.log(v%z)
+
+
+//  Suma 
+ let varible1 = 3
+ let varible2 = 4
+ console.log("suma")
+ console.log (varible1+varible2)
+// Multiplicacion
+let t = 43
+let k = 60
+console.log ("multiplicacion")
+console.log(t*k)
+
+// Exponenciacion
+let num = 13
+console.log ("Exponenciacion")
+console.log (num**4)
+
+
+// resta 
+// let num = 4
+// console.log ("resta")
+// console.log(num -2)
+
+// Divicion
+let x = 2
+console.log("Divicion")
+console.log(x /2)
+
+
+
+
+
+
+// Unary positivo 
+// let num = 2 ;
+// console.log("Unary positivo")
+// console.log (+num)
+
+
+// unary negation 
+// let num = 1 ;
+// console.log("Unary negation")
+// console.log (-num)
+
+
+
+// Decremento 
+// let num = 2
+// console.log("decremento")
+// console.log(--num)
+
+
+
+// pre-incremento 
+// let num = 1 ;
+// console.log("pre-incremento ");
+// console.log(--num);
+
+
+
+
+ // pos-Incremento 
+
+// let num= 1;
+// let inc = 1;
+// console.log ("Los Datos ingresados son de pos-incremento");
+// console.log (num);
+// num += inc ;
+// console.log ( num) ;
+// num += inc ;
+// console.log ( num) ;
